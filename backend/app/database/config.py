2021a6614fa3588c9b0f49775e89b8a44808ac0f
Arun Chandran <161107885+ArunChandran31@users.noreply.github.com>
2026-07-30 16:58:39 +0530

    Change database file name in config.py

--- backend/app/database/config.py
+++ backend/app/database/config.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-DATABASE_URL = f"sqlite:///{BASE_DIR}/api_copilot.db"
+DATABASE_URL = f"sqlite:///{BASE_DIR}/api_context_engine.db"
